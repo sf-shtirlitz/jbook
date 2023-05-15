60f5a4f3cd6c0430e30c1546bc379841ffbb0567
sf-shtirlitz <118398231+sf-shtirlitz@users.noreply.github.com>
2023-05-15 13:08:19 -0700

    bundler is finished

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
@@ -7,23 +7,35 @@ import Resizable from './resizable';
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
+  const [err, setErr] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+        try{
+            const output = await bundle(input);
+            setCode(output.code);
+            setErr(output.err);
+        } catch ( err) {
+
+        };
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
-        <div style={{height: "100%", width: "100%", display: "flex", flexDirection: "row"}}>
-            <Resizable direction="horizontal">
-                <CodeEditor
-                    initialValue="const a = 1;"
-                    onChange={(value) => setInput(value)}
-                />
-            </Resizable>
-            <Preview code={code} />
-        </div>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} err={err}/>
+      </div>
     </Resizable>
   );
 };
